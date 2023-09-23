96e33d8e982224bc2ef75dad6606d48d6cbc858d
Torin-Stacks <o.ajanaku@native.semicolon.africa>
2023-09-23 20:10:57 +0100

    initial commit

---
+++ src/userClass.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class UserClass extends React.Component{
+
+constructor(props){
+    super(props)
+}
+
+render(){
+    return (
+        <div><h1>Torin</h1></div>
+    )
+}
+
+
+}
+export default UserClass;
